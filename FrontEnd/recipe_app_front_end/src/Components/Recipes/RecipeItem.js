35cceb4b7f6a3dd2ff226bbbee22973a1d313039
Redbeard29 <ben@benshermanmusic.com>
2021-09-05 07:09:52 -0400

    set fixed card styling, set up different icons for food groups

--- FrontEnd/recipe_app_front_end/src/Components/Recipes/RecipeItem.js
+++ FrontEnd/recipe_app_front_end/src/Components/Recipes/RecipeItem.js
@@ -1,27 +1,44 @@
 import Card from 'react-bootstrap/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+import { faDrumstickBite } from '@fortawesome/free-solid-svg-icons';
+import { faFish } from '@fortawesome/free-solid-svg-icons';
+import { faCarrot } from '@fortawesome/free-solid-svg-icons';
 import { faStopwatch } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faBullseye } from '@fortawesome/free-solid-svg-icons';
 
 
 const RecipeItem = (props) => {
-    let quick_and_easy = null;
-    let is_favorite = null;
-    let have_made_before = null;
+    let quick_and_easy, is_favorite, have_made_before, meat;
+    quick_and_easy = is_favorite = have_made_before = meat = null;
 
     if(props.quick_and_easy === true){
-        quick_and_easy = <FontAwesomeIcon icon={ faStopwatch } size="3x" />
+        quick_and_easy = <FontAwesomeIcon icon={ faStopwatch } size="2x" />
     }
     if(props.is_favorite === true){
-        is_favorite = <FontAwesomeIcon icon={ faHeart } size="3x" />
+        is_favorite = <FontAwesomeIcon icon={ faHeart } size="2x" />
     }
     if(props.have_made_before === false){
-        have_made_before = <FontAwesomeIcon icon={ faBullseye } size="3x" />
+        have_made_before = <FontAwesomeIcon icon={ faBullseye } size="2x" />
+    }
+    
+    if(props.meat === 'RM'){
+        meat = <FontAwesomeIcon icon={ faHamburger } size="2x" />
+    }
+    else if(props.meat === 'PL'){
+        meat = <FontAwesomeIcon icon={ faDrumstickBite } size="2x" />
+    }
+    else if(props.meat === 'FI'){
+        meat = <FontAwesomeIcon icon={ faFish } size="2x" />
+    }
+    else{
+        meat = <FontAwesomeIcon icon={ faCarrot } size="2x" />
     }
 
+
     return (
-        <Card className="mb-3">
+        <Card className="h-100">
             <Card.Header>
                 <h1>
                     {props.title}
@@ -32,14 +49,16 @@ const RecipeItem = (props) => {
                     <h4>{props.image}</h4>
                 </div>
                 <div className="d-flex justify-content-center align-items-center">
-                    <h6 className="mx-1">{props.meat}</h6>
-                    <h6 className="mx-1">{props.category}</h6>
-                    <h6 className="mx-1">{props.rating}</h6>
-                    <div className="text-success mx-2">{quick_and_easy}</div>
-                    <div className="text-danger mx-2">{is_favorite}</div>
-                    <div className="text-warning mx-2">{have_made_before}</div>
+                    <h5 className="mx-1">{props.category}</h5>
+                    <h5 className="mx-1">{props.rating}</h5>
                 </div>
             </Card.Body>
+            <Card.Footer className="d-flex justify-content-center">
+                <div className="mx-1">{meat}</div>
+                <div className="text-success mx-2">{quick_and_easy}</div>
+                <div className="text-danger mx-2">{is_favorite}</div>
+                <div className="text-warning mx-2">{have_made_before}</div>
+            </Card.Footer>
         </Card>
     )
 }
--- FrontEnd/recipe_app_front_end/src/Components/Recipes/RecipeList.js
+++ FrontEnd/recipe_app_front_end/src/Components/Recipes/RecipeList.js
@@ -1,27 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import RecipeItem from './RecipeItem';
 
 const RecipeList = (props) => {
     return (
         <div className="recipe-list">
-            {props.recipes.map((recipe) => (
-                <RecipeItem
-                    key={recipe.id}
-                    title={recipe.title}
-                    image={recipe.picture}
-                    link={recipe.link_to_recipe}
-                    desc={recipe.description}
-                    meat={recipe.meat_type}
-                    category={recipe.category}
-                    best_quantity={recipe.best_quantity}
-                    rating={recipe.rating}
-                    comments={recipe.comments}
-                    last_made_at={recipe.last_made_at}
-                    quick_and_easy={recipe.quick_and_easy}
-                    is_favorite={recipe.is_favorite}
-                    have_made_before={recipe.have_made_before}
-                    user={recipe.saved_by}
-                />
-            ))}
+            <Container>
+                <Row>
+                    {props.recipes.map((recipe) => (
+                        <Col xs={4} className="py-2">
+                            <RecipeItem
+                                key={recipe.id}
+                                title={recipe.title}
+                                image={recipe.picture}
+                                link={recipe.link_to_recipe}
+                                desc={recipe.description}
+                                meat={recipe.meat_type}
+                                category={recipe.category}
+                                best_quantity={recipe.best_quantity}
+                                rating={recipe.rating}
+                                comments={recipe.comments}
+                                last_made_at={recipe.last_made_at}
+                                quick_and_easy={recipe.quick_and_easy}
+                                is_favorite={recipe.is_favorite}
+                                have_made_before={recipe.have_made_before}
+                                user={recipe.saved_by}
+                            />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </div>
     )
 };
